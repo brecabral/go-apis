3ce852837369698f8543b6ab8ef31f51f20c79a6
Breno Cabral <breno.ara.cabral@gmail.com>
2025-08-09 15:44:14 -0300

    feat: add create product endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/brecabral/go-apis/configs"
+	"github.com/brecabral/go-apis/internal/entity"
+	"github.com/brecabral/go-apis/internal/infra/database"
+	"github.com/brecabral/go-apis/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("teste.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+	http.HandleFunc("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":8080", nil)
 }
